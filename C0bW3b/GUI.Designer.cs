52b8606eadb99795ddd6cb9fcb2f8a700349ac88
Zebratic <zebraticyt@gmail.com>
2022-09-05 15:15:40 +0200

    Update GUI.Designer.cs

--- C0bW3b/GUI.Designer.cs
+++ C0bW3b/GUI.Designer.cs
@@ -48,8 +48,11 @@
             this.txtTarget = new System.Windows.Forms.TextBox();
             this.lblTarget = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numRecursiveLimit = new System.Windows.Forms.NumericUpDown();
+            this.lblRecursiveLimit = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numThreads)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numMinMatch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRecursiveLimit)).BeginInit();
             this.SuspendLayout();
             // 
             // txtHits
@@ -61,10 +64,10 @@
             this.txtHits.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtHits.Cursor = System.Windows.Forms.Cursors.Default;
             this.txtHits.ForeColor = System.Drawing.Color.White;
-            this.txtHits.Location = new System.Drawing.Point(132, 0);
+            this.txtHits.Location = new System.Drawing.Point(208, 61);
             this.txtHits.Name = "txtHits";
             this.txtHits.ReadOnly = true;
-            this.txtHits.Size = new System.Drawing.Size(402, 397);
+            this.txtHits.Size = new System.Drawing.Size(418, 313);
             this.txtHits.TabIndex = 0;
             this.txtHits.Text = "";
             this.txtHits.WordWrap = false;
@@ -77,9 +80,9 @@
             this.btnLoadDorks.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             this.btnLoadDorks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLoadDorks.ForeColor = System.Drawing.Color.White;
-            this.btnLoadDorks.Location = new System.Drawing.Point(12, 38);
+            this.btnLoadDorks.Location = new System.Drawing.Point(66, 100);
             this.btnLoadDorks.Name = "btnLoadDorks";
-            this.btnLoadDorks.Size = new System.Drawing.Size(114, 23);
+            this.btnLoadDorks.Size = new System.Drawing.Size(136, 23);
             this.btnLoadDorks.TabIndex = 1;
             this.btnLoadDorks.Text = "Load Dorks";
             this.btnLoadDorks.UseVisualStyleBackColor = false;
@@ -91,9 +94,9 @@
             this.btnLoadProxies.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             this.btnLoadProxies.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLoadProxies.ForeColor = System.Drawing.Color.White;
-            this.btnLoadProxies.Location = new System.Drawing.Point(12, 119);
+            this.btnLoadProxies.Location = new System.Drawing.Point(66, 181);
             this.btnLoadProxies.Name = "btnLoadProxies";
-            this.btnLoadProxies.Size = new System.Drawing.Size(114, 23);
+            this.btnLoadProxies.Size = new System.Drawing.Size(136, 23);
             this.btnLoadProxies.TabIndex = 2;
             this.btnLoadProxies.Text = "Load Proxies";
             this.btnLoadProxies.UseVisualStyleBackColor = false;
@@ -105,7 +108,7 @@
             this.cbRegexMatches.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.cbRegexMatches.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbRegexMatches.ForeColor = System.Drawing.Color.White;
-            this.cbRegexMatches.Location = new System.Drawing.Point(12, 96);
+            this.cbRegexMatches.Location = new System.Drawing.Point(66, 158);
             this.cbRegexMatches.Name = "cbRegexMatches";
             this.cbRegexMatches.Size = new System.Drawing.Size(103, 17);
             this.cbRegexMatches.TabIndex = 11;
@@ -118,7 +121,7 @@
             this.lblRetries.AutoSize = true;
             this.lblRetries.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.lblRetries.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.lblRetries.Location = new System.Drawing.Point(11, 339);
+            this.lblRetries.Location = new System.Drawing.Point(13, 495);
             this.lblRetries.Name = "lblRetries";
             this.lblRetries.Size = new System.Drawing.Size(52, 13);
             this.lblRetries.TabIndex = 10;
@@ -130,7 +133,7 @@
             this.lblBad.AutoSize = true;
             this.lblBad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.lblBad.ForeColor = System.Drawing.Color.Red;
-            this.lblBad.Location = new System.Drawing.Point(11, 323);
+            this.lblBad.Location = new System.Drawing.Point(13, 479);
             this.lblBad.Name = "lblBad";
             this.lblBad.Size = new System.Drawing.Size(38, 13);
             this.lblBad.TabIndex = 9;
@@ -142,7 +145,7 @@
             this.lblHits.AutoSize = true;
             this.lblHits.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.lblHits.ForeColor = System.Drawing.Color.Lime;
-            this.lblHits.Location = new System.Drawing.Point(11, 306);
+            this.lblHits.Location = new System.Drawing.Point(13, 462);
             this.lblHits.Name = "lblHits";
             this.lblHits.Size = new System.Drawing.Size(37, 13);
             this.lblHits.TabIndex = 8;
@@ -154,9 +157,9 @@
             this.btnLoadMatches.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             this.btnLoadMatches.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLoadMatches.ForeColor = System.Drawing.Color.White;
-            this.btnLoadMatches.Location = new System.Drawing.Point(12, 67);
+            this.btnLoadMatches.Location = new System.Drawing.Point(66, 129);
             this.btnLoadMatches.Name = "btnLoadMatches";
-            this.btnLoadMatches.Size = new System.Drawing.Size(114, 23);
+            this.btnLoadMatches.Size = new System.Drawing.Size(136, 23);
             this.btnLoadMatches.TabIndex = 7;
             this.btnLoadMatches.Text = "Load Matches";
             this.btnLoadMatches.UseVisualStyleBackColor = false;
@@ -167,7 +170,7 @@
             this.lblThreads.AutoSize = true;
             this.lblThreads.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.lblThreads.ForeColor = System.Drawing.Color.White;
-            this.lblThreads.Location = new System.Drawing.Point(9, 174);
+            this.lblThreads.Location = new System.Drawing.Point(63, 236);
             this.lblThreads.Name = "lblThreads";
             this.lblThreads.Size = new System.Drawing.Size(49, 13);
             this.lblThreads.TabIndex = 6;
@@ -178,7 +181,7 @@
             this.numThreads.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.numThreads.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.numThreads.ForeColor = System.Drawing.Color.White;
-            this.numThreads.Location = new System.Drawing.Point(59, 171);
+            this.numThreads.Location = new System.Drawing.Point(113, 233);
             this.numThreads.Maximum = new decimal(new int[] {
             400,
             0,
@@ -190,11 +193,11 @@
             0,
             0});
             this.numThreads.Name = "numThreads";
-            this.numThreads.Size = new System.Drawing.Size(67, 20);
+            this.numThreads.Size = new System.Drawing.Size(89, 20);
             this.numThreads.TabIndex = 5;
             this.numThreads.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numThreads.Value = new decimal(new int[] {
-            1,
+            10,
             0,
             0,
             0});
@@ -207,7 +210,7 @@
             this.cbProxyless.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cbProxyless.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbProxyless.ForeColor = System.Drawing.Color.White;
-            this.cbProxyless.Location = new System.Drawing.Point(12, 148);
+            this.cbProxyless.Location = new System.Drawing.Point(66, 210);
             this.cbProxyless.Name = "cbProxyless";
             this.cbProxyless.Size = new System.Drawing.Size(67, 17);
             this.cbProxyless.TabIndex = 4;
@@ -222,7 +225,7 @@
             this.btnStart.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnStart.ForeColor = System.Drawing.Color.White;
-            this.btnStart.Location = new System.Drawing.Point(12, 362);
+            this.btnStart.Location = new System.Drawing.Point(558, 485);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(114, 23);
             this.btnStart.TabIndex = 3;
@@ -236,7 +239,7 @@
             this.cbAllowDuplicates.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.cbAllowDuplicates.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbAllowDuplicates.ForeColor = System.Drawing.Color.White;
-            this.cbAllowDuplicates.Location = new System.Drawing.Point(12, 197);
+            this.cbAllowDuplicates.Location = new System.Drawing.Point(66, 259);
             this.cbAllowDuplicates.Name = "cbAllowDuplicates";
             this.cbAllowDuplicates.Size = new System.Drawing.Size(101, 17);
             this.cbAllowDuplicates.TabIndex = 12;
@@ -247,11 +250,9 @@
             // 
             this.cbLogFullURL.AutoSize = true;
             this.cbLogFullURL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
-            this.cbLogFullURL.Checked = true;
-            this.cbLogFullURL.CheckState = System.Windows.Forms.CheckState.Checked;
             this.cbLogFullURL.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.cbLogFullURL.ForeColor = System.Drawing.Color.White;
-            this.cbLogFullURL.Location = new System.Drawing.Point(12, 220);
+            this.cbLogFullURL.Location = new System.Drawing.Point(66, 282);
             this.cbLogFullURL.Name = "cbLogFullURL";
             this.cbLogFullURL.Size = new System.Drawing.Size(85, 17);
             this.cbLogFullURL.TabIndex = 13;
@@ -263,14 +264,14 @@
             this.numMinMatch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.numMinMatch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.numMinMatch.ForeColor = System.Drawing.Color.White;
-            this.numMinMatch.Location = new System.Drawing.Point(81, 243);
+            this.numMinMatch.Location = new System.Drawing.Point(141, 305);
             this.numMinMatch.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numMinMatch.Name = "numMinMatch";
-            this.numMinMatch.Size = new System.Drawing.Size(45, 20);
+            this.numMinMatch.Size = new System.Drawing.Size(61, 20);
             this.numMinMatch.TabIndex = 14;
             this.numMinMatch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numMinMatch.Value = new decimal(new int[] {
@@ -284,7 +285,7 @@
             this.lblMinMatch.AutoSize = true;
             this.lblMinMatch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.lblMinMatch.ForeColor = System.Drawing.Color.White;
-            this.lblMinMatch.Location = new System.Drawing.Point(9, 246);
+            this.lblMinMatch.Location = new System.Drawing.Point(63, 308);
             this.lblMinMatch.Name = "lblMinMatch";
             this.lblMinMatch.Size = new System.Drawing.Size(71, 13);
             this.lblMinMatch.TabIndex = 15;
@@ -295,9 +296,9 @@
             this.txtTarget.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
             this.txtTarget.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtTarget.ForeColor = System.Drawing.Color.White;
-            this.txtTarget.Location = new System.Drawing.Point(50, 12);
+            this.txtTarget.Location = new System.Drawing.Point(104, 74);
             this.txtTarget.Name = "txtTarget";
-            this.txtTarget.Size = new System.Drawing.Size(76, 20);
+            this.txtTarget.Size = new System.Drawing.Size(98, 20);
             this.txtTarget.TabIndex = 16;
             this.txtTarget.Text = "%ITEM%";
             // 
@@ -306,7 +307,7 @@
             this.lblTarget.AutoSize = true;
             this.lblTarget.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
             this.lblTarget.ForeColor = System.Drawing.Color.White;
-            this.lblTarget.Location = new System.Drawing.Point(9, 15);
+            this.lblTarget.Location = new System.Drawing.Point(63, 77);
             this.lblTarget.Name = "lblTarget";
             this.lblTarget.Size = new System.Drawing.Size(41, 13);
             this.lblTarget.TabIndex = 17;
@@ -316,23 +317,55 @@
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
-            this.checkBox1.Checked = true;
-            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
             this.checkBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.checkBox1.ForeColor = System.Drawing.Color.White;
-            this.checkBox1.Location = new System.Drawing.Point(12, 269);
+            this.checkBox1.Location = new System.Drawing.Point(66, 331);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(108, 17);
             this.checkBox1.TabIndex = 18;
             this.checkBox1.Text = "Recursive Search";
             this.checkBox1.UseVisualStyleBackColor = false;
             // 
+            // numRecursiveLimit
+            // 
+            this.numRecursiveLimit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.numRecursiveLimit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.numRecursiveLimit.ForeColor = System.Drawing.Color.White;
+            this.numRecursiveLimit.Location = new System.Drawing.Point(141, 354);
+            this.numRecursiveLimit.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numRecursiveLimit.Name = "numRecursiveLimit";
+            this.numRecursiveLimit.Size = new System.Drawing.Size(61, 20);
+            this.numRecursiveLimit.TabIndex = 19;
+            this.numRecursiveLimit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numRecursiveLimit.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // lblRecursiveLimit
+            // 
+            this.lblRecursiveLimit.AutoSize = true;
+            this.lblRecursiveLimit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.lblRecursiveLimit.ForeColor = System.Drawing.Color.White;
+            this.lblRecursiveLimit.Location = new System.Drawing.Point(63, 357);
+            this.lblRecursiveLimit.Name = "lblRecursiveLimit";
+            this.lblRecursiveLimit.Size = new System.Drawing.Size(79, 13);
+            this.lblRecursiveLimit.TabIndex = 20;
+            this.lblRecursiveLimit.Text = "Recusive Limit:";
+            // 
             // GUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
-            this.ClientSize = new System.Drawing.Size(534, 397);
+            this.ClientSize = new System.Drawing.Size(839, 525);
+            this.Controls.Add(this.numRecursiveLimit);
+            this.Controls.Add(this.lblRecursiveLimit);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.lblTarget);
             this.Controls.Add(this.txtTarget);
@@ -352,14 +385,16 @@
             this.Controls.Add(this.numThreads);
             this.Controls.Add(this.btnLoadMatches);
             this.Controls.Add(this.lblThreads);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(550, 370);
+            this.MinimumSize = new System.Drawing.Size(550, 450);
             this.Name = "GUI";
             this.Text = "C0bW3b";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GUI_FormClosing);
             this.Load += new System.EventHandler(this.GUI_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numThreads)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numMinMatch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRecursiveLimit)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -386,6 +421,8 @@
         private System.Windows.Forms.TextBox txtTarget;
         private System.Windows.Forms.Label lblTarget;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numRecursiveLimit;
+        private System.Windows.Forms.Label lblRecursiveLimit;
     }
 }
 
